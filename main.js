c2b4cb4f2173831e33e6cdf208ed48504caab690
anaClara3112 <130669098+anaClara3112@users.noreply.github.com>
2023-08-17 08:45:08 -0300

    commit da ana

---
+++ main.js
@@ -0,0 +1,13 @@
+let hora = new Date().getHours();
+let saudacao = "";
+
+if(hora < 12){
+    saudacao = "Bom Dia!";
+}else if(hora >= 12 && hora <= 18){
+    saudacao = "Boa Tarde!";
+}else{
+    saudacao = "Boa Noite!";
+}
+console.log(saudacao)
+
+document.getElementById("resultado").innerHTML = saudacao
